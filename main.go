7edc87c1539918e7189ca7a5c3beb5869e678b58
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 01:21:32 +0400

    Added a contact page

--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 func doNothing(res http.ResponseWriter, req *http.Request) {}
 
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL.Path)
 	res.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+	if req.URL.Path == "/" {
+		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+	} else if req.URL.Path == "/contact" {
+		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
+	}
 }
 
 func main() {
