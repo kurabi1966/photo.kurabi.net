7f1aa77c68360743ca4505e40568c78e5ea242ab
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 00:48:22 +0400

    Added a fresh config file

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"net/http"
 )
 
+func doNothing(res http.ResponseWriter, req *http.Request) {}
+
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
+	fmt.Println("Someone visited our page")
 	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/favicon.ico", doNothing)
 	http.ListenAndServe(":3333", nil)
-	// fmt.Println("Learnning GoLang!")
 }
