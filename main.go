58e8232f2f68cb899386d6db31b9a81fcf09c057
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 01:47:02 +0400

    Enhancing the way of handling favicon request

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"net/http"
 )
 
-func doNothing(res http.ResponseWriter, req *http.Request) {}
-
 func handlerFunc(res http.ResponseWriter, req *http.Request) {
 	fmt.Println(req.URL.Path)
 	res.Header().Set("Content-Type", "text/html")
@@ -14,6 +12,8 @@ func handlerFunc(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
 	} else if req.URL.Path == "/contact" {
 		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
+	} else if req.URL.Path == "favicon.ico" {
+		return
 	} else {
 		res.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(res, "<h1>Ooops... We could not find thepage you were looking for<h1>")
@@ -22,6 +22,5 @@ func handlerFunc(res http.ResponseWriter, req *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/favicon.ico", doNothing)
 	http.ListenAndServe(":3333", nil)
 }
