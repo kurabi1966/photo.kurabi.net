365b924248d09920c81719ce01c42eadb329eaa6
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 12:36:54 +0400

    Added Gorilla Mux package and 404 Handler

--- main.go
+++ main.go
@@ -3,24 +3,32 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(res http.ResponseWriter, req *http.Request) {
-	fmt.Println(req.URL.Path)
-	res.Header().Set("Content-Type", "text/html")
-	if req.URL.Path == "/" {
-		fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
-	} else if req.URL.Path == "/contact" {
-		fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
-	} else if req.URL.Path == "favicon.ico" {
-		return
-	} else {
-		res.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(res, "<h1>Ooops... We could not find thepage you were looking for<h1>")
-	}
+func Handle404(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(res, "<h1>Ooops... We could not find thepage you were looking for<h1>")
 }
 
+func home(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
+}
+
+func contact(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
+}
+
+func doNothing(res http.ResponseWriter, req *http.Request) {}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3333", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/favicon", doNothing)
+	r.NotFoundHandler = http.HandlerFunc(Handle404)
+	http.ListenAndServe(":3333", r)
 }
