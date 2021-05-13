f26e5625a5793f5fa98e54f03b353edefd8cc59e
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 13:44:20 +0400

    Added FAQ Page.

--- main.go
+++ main.go
@@ -22,12 +22,19 @@ func contact(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
 }
 
+func faq(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(res, "<h3>FAQ Page content goes here!</h3>")
+}
+
 func doNothing(res http.ResponseWriter, req *http.Request) {}
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+
 	r.HandleFunc("/favicon", doNothing)
 	r.NotFoundHandler = http.HandlerFunc(Handle404)
 	http.ListenAndServe(":3333", r)
