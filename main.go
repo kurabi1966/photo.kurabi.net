b25a4cac7acf535d55628880b1f7e3b4b5c87bee
Mohammed Ammar Kurabi <kurabi@gmail.com>
2021-05-13 13:54:28 +0400

    Trying Httprouter package instead of Gorilla package

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
 func Handle404(res http.ResponseWriter, req *http.Request) {
@@ -12,30 +12,30 @@ func Handle404(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(res, "<h1>Ooops... We could not find thepage you were looking for<h1>")
 }
 
-func home(res http.ResponseWriter, req *http.Request) {
+func home(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	res.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(res, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func contact(res http.ResponseWriter, req *http.Request) {
+func contact(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	res.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(res, "To get in touch, please send an email to <a href=\"mailto:support@photos.kurabi.net\">support@photos.kurabi.net</a>")
 }
 
-func faq(res http.ResponseWriter, req *http.Request) {
+func faq(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	res.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(res, "<h3>FAQ Page content goes here!</h3>")
 }
 
-func doNothing(res http.ResponseWriter, req *http.Request) {}
+func doNothing(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {}
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
 
-	r.HandleFunc("/favicon", doNothing)
-	r.NotFoundHandler = http.HandlerFunc(Handle404)
+	r.GET("/favicon", doNothing)
+	r.NotFound = http.HandlerFunc(Handle404)
 	http.ListenAndServe(":3333", r)
 }
